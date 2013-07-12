c5c75f8c84c1039a58738dc0f0716a308a857f6a
naheas <naheas@hanmail.net>
2013-07-12 17:01:45 +0900

    add first page

--- gen/com/example/twothree/R.java
+++ gen/com/example/twothree/R.java
@@ -21,18 +21,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int darkbluebutton=0x7f020000;
+        public static final int greenbutton=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int redbutton=0x7f020003;
+        public static final int rosemarybutton=0x7f020004;
+        public static final int yellowbutton=0x7f020005;
     }
     public static final class id {
-        public static final int TextView01=0x7f08000d;
-        public static final int action_settings=0x7f080012;
-        public static final int butFin=0x7f080011;
-        public static final int butNewGr=0x7f080009;
-        public static final int editGrName=0x7f08000a;
-        public static final int editMan=0x7f08000b;
-        public static final int editPlace=0x7f080010;
-        public static final int editWoman=0x7f08000e;
-        public static final int listView1=0x7f080008;
+        public static final int TextView01=0x7f080010;
+        public static final int action_settings=0x7f080015;
+        public static final int butFin=0x7f080014;
+        public static final int butNewGr=0x7f08000c;
+        public static final int editGrName=0x7f08000d;
+        public static final int editMan=0x7f08000e;
+        public static final int editPlace=0x7f080013;
+        public static final int editWoman=0x7f080011;
+        public static final int goinfo=0x7f08000a;
+        public static final int listView1=0x7f08000b;
+        public static final int mainbutton1=0x7f080009;
+        public static final int mainbutton2=0x7f080008;
         public static final int textGrName=0x7f080005;
         public static final int textMan=0x7f080004;
         public static final int textPlace=0x7f080007;
@@ -40,25 +48,28 @@ public final class R {
         public static final int textView2=0x7f080002;
         public static final int textView3=0x7f080001;
         public static final int textView4=0x7f080003;
-        public static final int textView5=0x7f08000c;
-        public static final int textView6=0x7f08000f;
+        public static final int textView5=0x7f08000f;
+        public static final int textView6=0x7f080012;
         public static final int textWoman=0x7f080006;
     }
     public static final class layout {
         public static final int activity_inform=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int activity_newgroup=0x7f030002;
+        public static final int activity_mtgroups=0x7f030002;
+        public static final int activity_newgroup=0x7f030003;
     }
     public static final class menu {
         public static final int inform=0x7f070000;
         public static final int main=0x7f070001;
-        public static final int newgroup=0x7f070002;
+        public static final int mtgroups=0x7f070002;
+        public static final int newgroup=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_inform=0x7f050004;
+        public static final int title_activity_mtgroups=0x7f050005;
         public static final int title_activity_newgroup=0x7f050003;
     }
     public static final class style {
--- src/com/example/twothree/MainActivity.java
+++ src/com/example/twothree/MainActivity.java
@@ -3,56 +3,18 @@ package com.example.twothree;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class MainActivity extends Activity{ 
-	private static final int NewGr_ACTIVITY = 1;
-	
-	private ArrayAdapter<String> _arrAdapter ;
-	
-	private mtGroup [] listGr = new mtGroup[30];
-	
-	private int listNum = 0;
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		settingListView();
 	}
-	
-	private void settingListView() {  
-        _arrAdapter = new ArrayAdapter<String>( getApplicationContext(), android.R.layout.simple_list_item_1 ) ;  
-        ListView listView = (ListView) findViewById( R.id.listView1 ) ;  
-        listView.setAdapter( _arrAdapter ) ;
-        
-        listView.setOnItemClickListener( new ListViewItemClickListener() );
-//        listView.setOnItemLongClickListener( new ListViewItemLongClickListener() );
-    }
-	
-	private class ListViewItemClickListener implements OnItemClickListener
-    {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
-        {
-        	Intent myintent = new Intent(MainActivity.this, InformActivity.class);
-    	    myintent.putExtra("name", listGr[position].name);
-    	    myintent.putExtra("man", listGr[position].man);
-    	    myintent.putExtra("woman", listGr[position].woman);
-    	    myintent.putExtra("place", listGr[position].place);
-    	    startActivity(myintent);
-        }        
-    }
 	
  
 	@Override
@@ -62,81 +24,12 @@ public class MainActivity extends Activity{
 		return true;
 	}
 	
-	public void go (View v) {
-
-	    Intent myintent = new Intent(this, NewgroupActivity.class);
-	    startActivityForResult(myintent, NewGr_ACTIVITY);
+	public void goo (View v) {
+	    Intent mtintent = new Intent(MainActivity.this, MtgroupsActivity.class);
+	    startActivity(mtintent);
 	}
 	
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
-    {		
-        if(resultCode == RESULT_OK && requestCode == NewGr_ACTIVITY) {        	
-        	String nameStr = data.getExtras().getString("name");
-    		String manStr = data.getExtras().getString("man");
-    		String womanStr = data.getExtras().getString("woman");
-    		String placeStr = data.getExtras().getString("place");
-    		listGr[listNum] = new mtGroup(nameStr, manStr, womanStr, placeStr);
-    		listNum++;
-    		
-    		refresh(nameStr);
-        }
-    }
 
-	
-	private void refresh( String inputValue ) {  
-        _arrAdapter.add( inputValue ) ;  
-        _arrAdapter.notifyDataSetChanged() ;  
-    }
-	
-	
-	
-    
-   /*
-    * ListView의 item을 길게 클릭했을 경우.
-    * 클릭된 item을 삭제한다.
-    * @author stargatex
-    *
-   private class ListViewItemLongClickListener implements OnItemLongClickListener
-   {
-       @Override
-       public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) 
-       {
-           String selectedStr = listGr[position].name;
-           AlertDialog.Builder alertDlg = new AlertDialog.Builder(view.getContext());
-           alertDlg.setTitle(R.string.alert_title_question);
-
-           // '예' 버튼이 클릭되면
-           alertDlg.setPositiveButton( R.string.button_yes, new DialogInterface.OnClickListener()
-           {
-                @Override
-                public void onClick( DialogInterface dialog, int which ) 
-                {
-               	 _arrAdapter.remove();
-                    
-                    // 아래 method를 호출하지 않을 경우, 삭제된 item이 화면에 계속 보여진다.
-                    adapter.notifyDataSetChanged();                     
-                    dialog.dismiss();  // AlertDialog를 닫는다.
-                }
-           });
-           
-           // '아니오' 버튼이 클릭되면
-           alertDlg.setNegativeButton( R.string.button_no, new DialogInterface.OnClickListener()
-           {
-                @Override
-                public void onClick( DialogInterface dialog, int which ) {
-                    dialog.dismiss();  // AlertDialog를 닫는다.
-                }
-           });
-           
-           
-           alertDlg.setMessage( String.format( getString(R.string.alert_msg_delete), _arrAdapter.getItem(position)) );
-           alertDlg.show();
-           return false;
-       }
-    
-   }
-   
-   */
 
 
 }
